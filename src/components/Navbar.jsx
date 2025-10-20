6a9b2aab4d56d81b8016860385fc8528a8e5502d
chayansrk64 <chayansrk64@gmail.com>
2025-10-20 09:31:15 +0600

    firebase added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { use, useContext } from 'react';
 import { Link, NavLink } from 'react-router';
-import user from '../assets/demo-user.png'
+import userIcon from '../assets/demo-user.png'
+import { AuthContext } from '../provider/AuthProvider';
 
 const Navbar = () => {
+
+    const { user } = use(AuthContext)
+
     return (
         <nav className='flex justify-between items-center w-11/12 mx-auto my-3'>
-            <div></div>
+            <div>{user && user.email}</div>
             <div className='flex gap-3'>
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="/about">About</NavLink>
                 <NavLink to="/career">Career</NavLink>
             </div>
             <div className='flex gap-4'>
-                <img src={user} alt="" />
+                <img src={userIcon} alt="" />
                 <Link to="/auth/login" className='btn btn-primary px-4'>Log In</Link>
             </div>
             
