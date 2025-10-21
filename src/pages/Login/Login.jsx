2ac7f7f4d92959e89e194383ebacff4676f36ed2
chayansrk64 <chayansrk64@gmail.com>
2025-10-21 10:37:29 +0600

    location added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,13 @@
 import React, { use } from 'react';
-import { Link } from 'react-router';
+import { Link, useLocation, useNavigate } from 'react-router';
 import { AuthContext } from '../../provider/AuthProvider';
 
 const Login = () => {
 
     const { logIn } = use(AuthContext)
+    const location = useLocation();
+    const navigate = useNavigate()
+    console.log(location);
 
     const handleLogIn = (e) => {
         e.preventDefault()
@@ -15,6 +18,7 @@ const Login = () => {
         .then(result => {
             const user = result.user;
             console.log('user=>', user);
+            navigate(`${location.state ? location.state : "/"}`)
         })
         .catch((error) => {
             const errorCode = error.code;
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,10 +1,12 @@
 import React, { use } from 'react';
-import { Navigate } from 'react-router';
+import { Navigate, useLocation } from 'react-router';
 import { AuthContext } from '../provider/AuthProvider';
 import Loading from '../components/Loading';
 
 const PrivateRoute = ({children}) => {
     const {user, loading} = use(AuthContext);
+    const location = useLocation()
+    console.log(location);
     
     if(loading){
         return <Loading></Loading>
@@ -14,7 +16,7 @@ const PrivateRoute = ({children}) => {
         return children
     }
   
-    return <Navigate to="/auth/login"></Navigate>
+    return <Navigate state={location.pathname} to="/auth/login"></Navigate>
   
 };
 
